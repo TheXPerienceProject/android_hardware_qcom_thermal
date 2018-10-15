38d9704cb4894b6b52caed50f0e2eab088ba2a73
klozz <shaclone707@gmail.com>
2018-10-15 09:53:27 -0400

    use project pathmap
    Signed-off-by: klozz <shaclone707@gmail.com>

--- Android.mk
+++ Android.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+ifeq ($(call my-dir),$(call project-path-for,qcom-thermal))
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -37,3 +37,4 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
+endif
